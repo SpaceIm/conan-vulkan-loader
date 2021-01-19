58d8ff791eeeb49d2e2e0bd64cad76ad60850623
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-01-19 14:16:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# conan-vulkan-loader
+
+## Package Status
+
+| Windows | Linux | macOS |
+|:-------:|:-----:|:-----:|
+|[![Build status](https://ci.appveyor.com/api/projects/status/jlbusom4ks941pn2/branch/testing%2F1.2.154.0?svg=true)](https://ci.appveyor.com/project/SpaceIm/conan-vulkan-loader)|[![Build status](https://github.com/SpaceIm/conan-vulkan-loader/workflows/.github/workflows/conan.yml/badge.svg?branch=testing%2F1.2.154.0)](https://github.com/SpaceIm/conan-vulkan-loader/actions?query=branch%3Atesting%2F1.2.154.0)|[![Build Status](https://travis-ci.com/SpaceIm/conan-vulkan-loader.svg?branch=testing%2F1.2.154.0)](https://travis-ci.com/SpaceIm/conan-vulkan-loader)|
+
+## Description
+
+[Conan.io](https://conan.io) package for [Vulkan-Loader](https://github.com/KhronosGroup/Vulkan-Loader).
---
+++ conanfile.py
@@ -0,0 +1,118 @@
+from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
+import glob
+import os
+
+
+class VulkanLoaderConan(ConanFile):
+    name = "vulkan-loader"
+    description = "Khronos official Vulkan ICD desktop loader for Windows, Linux, and MacOS."
+    license = "Apache-2.0"
+    topics = ("conan", "vulkan-loader", "vulkan", "loader", "desktop", "gpu")
+    homepage = "https://github.com/KhronosGroup/Vulkan-Loader"
+    url = "https://github.com/conan-io/conan-center-index"
+
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "with_wsi_xcb": [True, False],
+        "with_wsi_xlib": [True, False],
+        "with_wsi_wayland": [True, False],
+        "with_wsi_directfb": [True, False],
+    }
+    default_options = {
+        "shared": True,
+        "with_wsi_xcb": True,
+        "with_wsi_xlib": True,
+        "with_wsi_wayland": True,
+        "with_wsi_directfb": False,
+    }
+
+    exports_sources = "CMakeLists.txt"
+    generators = "cmake"
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+        if self.settings.os != "Linux":
+            del self.options.with_wsi_xcb
+            del self.options.with_wsi_xlib
+            del self.options.with_wsi_wayland
+            del self.options.with_wsi_directfb
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+        del self.settings.compiler.libcxx
+        del self.settings.compiler.cppstd
+        if self.options.get_safe("with_wsi_directfb"):
+            # TODO: directfb package
+            raise ConanInvalidConfiguration("Conan recipe for DirectFB is not available yet.")
+        if not tools.is_apple_os(self.settings.os) and not self.options.shared:
+            raise ConanInvalidConfiguration("Static builds are not supported on {}".format(self.settings.os))
+
+    def requirements(self):
+        self.requires("vulkan-headers/{}".format(self.version))
+        if self.options.get_safe("with_wsi_xcb") or self.options.get_safe("with_wsi_xlib"):
+            self.requires("xorg/system")
+        if self.options.get_safe("with_wsi_wayland"):
+            self.requires("wayland/1.18.0")
+
+    def package_id(self):
+        if self.info.settings.compiler == "Visual Studio":
+            self.info.settings.compiler.runtime = str(self.info.settings.compiler.runtime).replace("MD", "MT")
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        os.rename(glob.glob("Vulkan-Loader-*")[0], self._source_subfolder)
+
+    def build(self):
+        self._patch_sources()
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def _patch_sources(self):
+        tools.replace_in_file(os.path.join(self._source_subfolder, "cmake", "FindVulkanHeaders.cmake"),
+                              "HINTS ${VULKAN_HEADERS_INSTALL_DIR}/share/vulkan/registry",
+                              "HINTS ${VULKAN_HEADERS_INSTALL_DIR}/res/vulkan/registry")
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.definitions["VULKAN_HEADERS_INSTALL_DIR"] = self.deps_cpp_info["vulkan-headers"].rootpath
+        self._cmake.definitions["BUILD_TESTS"] = False
+        if tools.is_apple_os(self.settings.os):
+            self._cmake.definitions["BUILD_STATIC_LOADER"] = not self.options.shared
+        self._cmake.definitions["USE_CCACHE"] = False
+        if self.settings.os == "Linux":
+            self._cmake.definitions["BUILD_WSI_XCB_SUPPORT"] = self.options.with_wsi_xcb
+            self._cmake.definitions["BUILD_WSI_XLIB_SUPPORT"] = self.options.with_wsi_xlib
+            self._cmake.definitions["BUILD_WSI_WAYLAND_SUPPORT"] = self.options.with_wsi_wayland
+            self._cmake.definitions["BUILD_WSI_DIRECTFB_SUPPORT"] = self.options.with_wsi_directfb
+        if self.settings.os == "Windows":
+            self._cmake.definitions["ENABLE_WIN10_ONECORE"] = False
+        self._cmake.definitions["BUILD_LOADER"] = True
+        self._cmake.configure()
+        return self._cmake
+
+    def package(self):
+        self.copy("LICENSE.txt", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        tools.rmdir(os.path.join(self.package_folder, "loader"))
+
+    def package_info(self):
+        self.cpp_info.names["pkg_config"] = "vulkan"
+        suffix = "-1" if self.settings.os == "Windows" else ""
+        self.cpp_info.libs = ["vulkan" + suffix]
+        if self.settings.os == "Linux":
+            self.cpp_info.system_libs = ["dl", "pthread", "m"]
+        elif self.settings.os == "Macos":
+            self.cpp_info.frameworks = ["CoreFoundation"]
